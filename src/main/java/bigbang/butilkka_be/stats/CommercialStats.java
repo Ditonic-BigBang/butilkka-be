b4c6fc219a3c071f786749619912220e045204ce
chaegong <138372298+chaechae23011667@users.noreply.github.com>
2026-07-08 15:47:45 +0900

    fix: CommercialStats @Setter 추가

--- src/main/java/bigbang/butilkka_be/stats/CommercialStats.java
+++ src/main/java/bigbang/butilkka_be/stats/CommercialStats.java
@@ -4,12 +4,14 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "commercial_stats")
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CommercialStats {
 
